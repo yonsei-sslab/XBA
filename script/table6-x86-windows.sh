44a9ab7e682b61267113e59efaf5cb7de6c938b8
Geunwoo Kim <posgnu@gmail.com>
2022-04-25 07:31:53 +0000

    Udate scripts

--- script/table6-x86-windows.sh
+++ script/table6-x86-windows.sh
@@ -4,27 +4,27 @@ EMBEDDING_LIST="bow deepbindiff"
 
 program="curl"
 epochs=8000
-python train.py --k 100 --layer 5 --target $program --embedding_type bow --seed 5 --epochs $epochs
-python train.py --k 100 --layer 5 --target $program --embedding_type deepbindiff --seed 5 --epochs $epochs
+python train.py --k 100 --layer 3 --target $program --embedding_type bow --seed 5 --epochs $epochs
+python train.py --k 100 --layer 3 --target $program --embedding_type deepbindiff --seed 5 --epochs $epochs
 
 program="openssl"
 epochs=8000
-python train.py --k 100 --layer 5 --target $program --embedding_type bow --seed 5 --epochs $epochs
-python train.py --k 100 --layer 5 --target $program --embedding_type deepbindiff --seed 5 --epochs $epochs
+python train.py --k 100 --layer 3 --target $program --embedding_type bow --seed 5 --epochs $epochs
+python train.py --k 100 --layer 3 --target $program --embedding_type deepbindiff --seed 5 --epochs $epochs
 
 program="sqlite3"
 epochs=8000
-python train.py --k 100 --layer 5 --target $program --embedding_type bow --seed 5 --epochs $epochs
-python train.py --k 100 --layer 5 --target $program --embedding_type deepbindiff --seed 5 --epochs $epochs
+python train.py --k 100 --layer 3 --target $program --embedding_type bow --seed 5 --epochs $epochs
+python train.py --k 100 --layer 3 --target $program --embedding_type deepbindiff --seed 5 --epochs $epochs
 
 program="httpd"
 epochs=8000
-python train.py --k 100 --layer 5 --target $program --embedding_type bow --seed 5 --epochs $epochs
-python train.py --k 100 --layer 5 --target $program --embedding_type deepbindiff --seed 5 --epochs $epochs
+python train.py --k 100 --layer 3 --target $program --embedding_type bow --seed 5 --epochs $epochs
+python train.py --k 100 --layer 3 --target $program --embedding_type deepbindiff --seed 5 --epochs $epochs
 comment
 
 program="libcrypto"
 epochs=15000
-python train.py --k 50 --layer 5 --target $program --embedding_type bow --seed 5 --epochs $epochs
-# python train.py --k 50 --layer 5 --target $program --embedding_type deepbindiff --seed 5 --epochs $epochs
+python train.py --k 50 --layer 3 --target $program --embedding_type bow --seed 5 --epochs $epochs
+python train.py --k 50 --layer 3 --target $program --embedding_type deepbindiff --seed 5 --epochs $epochs
 
