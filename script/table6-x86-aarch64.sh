8af08322c71b0f559e12e8416ffe1f7945b4dbe1
Geunwoo Kim <posgnu@gmail.com>
2022-04-26 17:56:03 +0000

    Set libcrypto epoch 15000

--- script/table6-x86-aarch64.sh
+++ script/table6-x86-aarch64.sh
@@ -3,12 +3,12 @@ PROGRAM_LIST="libc libcrypto-xarch"
 EMBEDDING_LIST="bow deepbindiff"
 
 program="libcrypto-xarch"
-epochs=10000
-python train.py --k 50 --layer 5 --target $program --embedding_type bow --seed 5 --epochs $epochs
-python train.py --k 50 --layer 5 --target $program --embedding_type deepbindiff --seed 5 --epochs $epochs
+epochs=15000
+python train.py --k 50 --layer 3 --target $program --embedding_type bow --seed 5 --epochs $epochs
+python train.py --k 50 --layer 3 --target $program --embedding_type deepbindiff --seed 5 --epochs $epochs
 
 program="libc"
-epochs=15000
-python train.py --k 100 --layer 5 --target $program --embedding_type bow --seed 5 --epochs $epochs
-python train.py --k 100 --layer 5 --target $program --embedding_type deepbindiff --seed 5 --epochs $epochs
+epochs=10000
+python train.py --k 100 --layer 3 --target $program --embedding_type bow --seed 5 --epochs $epochs
+python train.py --k 100 --layer 3 --target $program --embedding_type deepbindiff --seed 5 --epochs $epochs
 
